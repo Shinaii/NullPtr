587073a79234e670d308ae51b63c3b160c627bf1
Shinaii <contact@shinaii.xyz>
2025-05-19 09:31:18 +0200

    fix: Update footer iconURL handling in EmbedUtils to use undefined instead of an empty string

--- apps/Discord/src/lib/utils/embedUtils.ts
+++ apps/Discord/src/lib/utils/embedUtils.ts
@@ -4,6 +4,6 @@ import { Command } from '@sapphire/framework';
 
 export class EmbedUtils {
     public static setFooter(embed: EmbedBuilder, interaction: Command.ChatInputCommandInteraction): EmbedBuilder {
-        return embed.setFooter({ text: 'NullPtr by Shinaii', iconURL: interaction.client.user.avatarURL() ?? '' });
+        return embed.setFooter({ text: 'NullPtr by Shinaii', iconURL: interaction.client.user.avatarURL() ?? undefined });
     }
 }
